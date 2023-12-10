Add normalization through callable function. Fix incompatibility with predefined loss functions

angionet/datasets/sennet.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Any, Sequence
 
 import albumentations as A
 import cv2
@@ -7,7 +7,8 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 
-from ..functional import decode
+from ..functional import decode, rescale
+from ..utils import unbind
 
 
 class TrainDataset(Dataset):
@@ -20,6 +21,9 @@ class TrainDataset(Dataset):
         List of file paths to train images.
     transforms : A.BaseCompose
         Albumentations Compose object.
+    normalize : callable, optional
+        The normalization function. If not specified, perform min-max normalization
+        as default.
 
     Attributes
     ----------
@@ -27,32 +31,37 @@ class TrainDataset(Dataset):
         List of file paths to train images.
     transforms : A.BaseCompose
         Albumentations Compose object.
+    normalize : callable, optional
+        The normalization function. By default perform min-max normalization.
     """
 
-    def __init__(self, paths: Sequence[str], transforms: A.BaseCompose):
+    def __init__(
+        self,
+        paths: Sequence[str],
+        transforms: A.BaseCompose,
+        normalize: Any = None,
+    ):
         self.paths = paths
         self.transforms = transforms
+        self.normalize = normalize if normalize is not None else rescale
 
     def __len__(self) -> int:
         return len(self.paths)
 
     def __getitem__(self, index: int) -> tuple[torch.Tensor, ...]:
-        image, masks, dtms = [
+        batch = [
             np.asarray(item, dtype=np.float32)
             for item in np.load(self.paths[index]).values()
         ]
 
-        augs = self.transforms(
-            image=image, masks=[masks[0], masks[1], dtms[0], dtms[1]]
-        )
+        augs = self.transforms(image=batch[0], masks=[*unbind(np.stack(batch[1:]), 0)])
+        image = self.normalize(augs["image"])
+        masks = [torch.stack(augs["masks"][:len(batch[1])])]
 
-        image = augs["image"]
-        masks = torch.stack(augs["masks"][:2])
-        dtms = torch.stack(augs["masks"][2:])
+        if len(augs["masks"]) > 2:
+            masks.append(torch.stack(augs["masks"][len(batch[1]):]))
 
-        image = (image - image.min()) / (image.max() - image.min() + 1e-6)
-
-        return image, masks, dtms
+        return image, *masks
 
 
 class InferenceDataset(Dataset):
@@ -65,15 +74,17 @@ class InferenceDataset(Dataset):
         List of file paths to test images.
     transforms : A.BaseCompose
         Albumentations Compose with image augmentations.
+    normalize : callable, optional
+        The normalization function. If not specified, perform min-max normalization
+        as default.
     """
 
     def __init__(
-        self,
-        paths: Sequence[str],
-        transforms: A.BaseCompose,
+        self, paths: Sequence[str], transforms: A.BaseCompose, normalize: Any = None
     ):
         self.paths = paths
         self.transforms = transforms
+        self.normalize = normalize if normalize is not None else rescale
 
     def __len__(self) -> int:
         return len(self.paths)
@@ -83,7 +94,7 @@ class InferenceDataset(Dataset):
         image = np.asarray(image, np.float32)
 
         image = self.transforms(image=image)["image"]
-        image = (image - image.mean()) / (image.std() + 1e-6)
+        image = self.normalize(image)
 
         return image
 