899b0febbe07a32c0266e009cd6384db15e943e2
recursingfeynman <chasedflea089@icloud.com>
2023-12-12 18:33:20 +0000

    Add standard scaler

--- angionet/functional/normalization.py
+++ angionet/functional/normalization.py
@@ -1,30 +1,17 @@
-from typing import Optional
 
-import numpy as np
-import torch
+def standardize(image, stats = None):
+    if stats is not None:
+        image = (image - stats[0]) / stats[1]
+    else:
+        image = (image - image.mean()) / image.std()
+
+    return image
 
 
-def rescale(
-    image: torch.Tensor | np.ndarray, stats: Optional[tuple[float, float]] = None
-) -> torch.Tensor | np.ndarray:
-    """
-    Rescale input image using min-max normalization technique.
-
-    Parameters
-    ----------
-    image : array-like
-        Input image to rescale.
-    stats : tuple of floats, optional
-        Normalization statistics (min, max). If not specified, scaling will be
-        done based on the minimum and maximum values of the input image.
-
-    Returns
-    -------
-    array-like
-        Rescaled image.
-    """
+def rescale(image, stats = None):
     if stats is not None:
         image = (image - stats[0]) / (stats[1] - stats[0])
     else:
         image = (image - image.min()) / (image.max() - image.min())
+
     return image
