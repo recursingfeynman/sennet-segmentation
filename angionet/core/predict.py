ae325de46b661c804391cc9c6d20f40c0e577274
recursingfeynman <chasedflea089@icloud.com>
2023-12-12 19:39:08 +0000

    Move dataset initialization outside predict logic

--- angionet/core/predict.py
+++ angionet/core/predict.py
@@ -3,17 +3,16 @@ from typing import Any
 import numpy as np
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 
-from ..datasets.sennet import InferenceDataset
 from ..functional import combine_patches, extract_patches
 
 
 @torch.no_grad()
 def predict(
     model: nn.Module,
-    paths: list[str],
+    dataset: Dataset,
     device: str | torch.device,
     config: Any,
 ) -> np.ndarray:
@@ -24,12 +23,12 @@ def predict(
     ----------
     model : nn.Module
         Trained model.
-    paths : list of str
-        Paths to source images (D,).
+    dataset : Dataset
+        Inference dataset.
     device : str or torch.device
         Device on which to perform predictions.
     config : any
-        The configuration class. Must have transforms, dim, stride, padding, batch_size,
+        The configuration class. Must have dim, stride, padding, batch_size,
         thresholds and lomc attributes.
 
     Returns
@@ -37,7 +36,6 @@ def predict(
     np.array
         Predicted volume [D, H, W].
     """
-    transforms = config.transforms
     dim = config.dim
     stride = config.stride
     padding = config.padding
@@ -48,7 +46,6 @@ def predict(
     model.eval()
     nthreads = torch.get_num_threads() * 2
     volume = []
-    dataset = InferenceDataset(paths, transforms)
     loader = DataLoader(dataset, batch_size=bs, num_workers=nthreads)
     for images in tqdm(loader, desc="Processing"):
         B, C, H, W = images.shape
@@ -60,7 +57,7 @@ def predict(
 
         outputs = outputs.sigmoid().cpu()
         outputs = outputs.contiguous().view(B, -1, outputs.size(1), dim, dim)
-        outputs = (outputs[:, :, 0:1] * outputs[:, :, 1:2]) > thresholds[0] # V * K
+        outputs = (outputs[:, :, 0:1] * outputs[:, :, 1:2]) > thresholds[0]  # V * K
 
         # Reconstruct original images
         outputs = combine_patches((H, W), outputs.byte(), dim, stride, lomc)
